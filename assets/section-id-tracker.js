43778a8808ef25ff9592b70da906cf39d92fbf39
Leland Reardon <leland6925@gmail.com>
2024-08-28 13:44:53 -0700

    .

--- assets/section-id-tracker.js
+++ assets/section-id-tracker.js
@@ -1,13 +1,12 @@
 
-if (typeof window.sectionTrackerInitialized === 'undefined') {
-    window.sectionTrackerInitialized = true;
+if (typeof window.variantTrackerInitialized === 'undefined') {
+    window.variantTrackerInitialized = true;
 
-    let currentSection = localStorage.getItem('currentSection') || null;
+    let rememberedVariant = localStorage.getItem('rememberedVariant') || null;
     let sectionVisibility = {};
     let observer;
     let lastSection;
-    console.log('__Section-Tracker Previous Section: ' + lastSection);
- const debounce = (func, wait) => {
+ 	const debounce = (func, wait) => {
         let timeout;
         return (...args) => {
             clearTimeout(timeout);
@@ -40,96 +39,68 @@ if (typeof window.sectionTrackerInitialized === 'undefined') {
         gazer.observe(document.body, { childList: true, subtree: true });
     }
 
-    function initializeObserver() {
-        observer = new IntersectionObserver((entries) => {
-            console.log('observer active');
-            entries.forEach(entry => {
-                const sectionId = entry.target.id;
-                if (entry.isIntersecting) {
-                    sectionVisibility[sectionId] = entry.intersectionRatio;
-                } else {
-                    delete sectionVisibility[sectionId];
-                }
+    // function initializeObserver() {
+    //     observer = new IntersectionObserver((entries) => {
+    //         console.log('observer active');
+    //         entries.forEach(entry => {
+    //             const sectionId = entry.target.id;
+    //             if (entry.isIntersecting) {
+    //                 sectionVisibility[sectionId] = entry.intersectionRatio;
+    //             } else {
+    //                 delete sectionVisibility[sectionId];
+    //             }
+    //         });
+    //     }, { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] });
+    // }
 
-                updateCurrentSection();
-            });
-        }, { threshold: [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1] });
+	function updateRememberedVariant(variantId) {
+		console.log(`[TRACK] ${variantId}`);
+	}
+
+	function handlePageTransition() {
+			// console.log('handlePageTransition Called');
+			// Store the current section before navigation
+			// console.log('__Observer-Page-Transition Current Section:' + rememberedVariant);
+			// console.log('__Observer-Page-Transition Last Section:' + localStorage.getItem('rememberedVariant'));
+			lastSection = rememberedVariant || localStorage.getItem('rememberedVariant');
+			// console.log(`Handling page transition to:  ${lastSection}`);
+			cleanupObserver();
+	}
+
+	function cleanupObserver() {
+			// console.log('clean observer called');
+			if (observer) {
+					observer.disconnect();
+					observer = null;
+					sectionVisibility = {};
+			}
     }
 
-function updateCurrentSection() {
-    const mostVisibleSection = Object.entries(sectionVisibility).reduce((max, [id, ratio]) => {
-        return ratio > max.ratio ? { id, ratio } : max;
-    }, { id: null, ratio: 0 });
+    // const allowedPaths = ['/', '/#home', '/#tees', '/#sweaters', '/#bags', '/'];
 
-    if (mostVisibleSection.id !== currentSection) {
-        if (mostVisibleSection.id !== null) {
-            currentSection = mostVisibleSection.id;
-            // Store the current section in localStorage
-            localStorage.setItem('currentSection', currentSection);
-            
-            // Dispatch the event with the new current section
-            window.dispatchEvent(new CustomEvent('currentSectionChanged', { detail: currentSection }));
-            window.addEventListener('currentSectionChanged', (event) => {
-    console.log('~~~~Current section changed to:', event.detail);
-    // Perform any necessary actions here
-});
+	function initializeSectionTracker() {
+			// const currentPath = window.location.pathname;
+			// const currentHostname = window.location.hostname;
+			
+			// Check if the current page is the root path
+	//     if (allowedPaths.includes(currentPath) && currentHostname === window.location.hostname) {
 
-        } else {
-            console.log('No section currently visible');
-            // Don't update localStorage here
-        }
-    }
-}
+	//         const sections = document.querySelectorAll('section');
+	//         sections.forEach(section => {
+	//             if (section.id) {
+	//                 console.log(`+++Observing section: ${section.id}`);
+	//                 observer.observe(section);
+	//             } else {
+	//                 console.warn('Found a section without an id attribute');
+	//             }
+	//         });
+	//     } else {
+	//         console.log('Section tracker is not initialized on this page.');
+	//     }
+	}
 
-function updateTrackedProduct(product) {
-	console.log(`[TRACK] ${product}`);
-}
-
-    function handlePageTransition() {
-        console.log('handlePageTransition Called');
-        // Store the current section before navigation
-        console.log('__Observer-Page-Transition Current Section:' + currentSection);
-        console.log('__Observer-Page-Transition Last Section:' + localStorage.getItem('currentSection'));
-        lastSection = currentSection || localStorage.getItem('currentSection');
-        console.log(`Handling page transition to:  ${lastSection}`);
-        cleanupObserver();
-    }
-
-    function cleanupObserver() {
-        // console.log('clean observer called');
-        if (observer) {
-            observer.disconnect();
-            observer = null;
-            sectionVisibility = {};
-        }
-    }
-
-    const allowedPaths = ['/', '/#home', '/#tees', '/#sweaters', '/#bags', '/'];
-
-    function initializeSectionTracker() {
-        const currentPath = window.location.pathname;
-        const currentHostname = window.location.hostname;
-        
-        // Check if the current page is the root path
-        if (allowedPaths.includes(currentPath) && currentHostname === window.location.hostname) {
-
-            const sections = document.querySelectorAll('section');
-            sections.forEach(section => {
-                if (section.id) {
-                    console.log(`+++Observing section: ${section.id}`);
-                    observer.observe(section);
-                } else {
-                    console.warn('Found a section without an id attribute');
-                }
-            });
-        } else {
-            console.log('Section tracker is not initialized on this page.');
-        }
-    }
-
-    window.addEventListener('load', () => {
-        initializeObserver();
-        initializeSectionTracker();
-    });
-   
+	window.addEventListener('load', () => {
+			initializeObserver();
+			initializeSectionTracker();
+	});
 }
