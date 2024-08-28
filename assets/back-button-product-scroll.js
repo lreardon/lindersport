5769f22cea383f8ad6ecf89ac681c39f5e837dca
Leland Reardon <leland6925@gmail.com>
2024-08-28 13:56:30 -0700

    .

--- assets/back-button-product-scroll.js
+++ assets/back-button-product-scroll.js
@@ -1,33 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
   const rememberedVariant = localStorage.getItem('rememberedVariant');
 	console.log('rememberedVariant:', rememberedVariant);
-  // const pollForElement = (selector, interval = 100, maxAttempts = 50) => {
-  //   let attempts = 0;
-
-  //   const poller = setInterval(() => {
-  //     const targetElement = document.getElementById(selector);
-  //     attempts++;
-  //     console.log('attempts' + attempts)
-  //     if (targetElement) {
-  //       targetElement.scrollIntoView({ behavior: 'smooth' });
-  //       clearInterval(poller); // Stop polling once the element is found
-  //     }
-
-  //     if (attempts >= maxAttempts) {
-  //       clearInterval(poller); // Stop polling after max attempts
-  //     }
-  //   }, interval);
-  // };
-
-  // const handleHashNavigation = () => {
-  //   const targetHash = window.location.hash; // e.g., '#targetId'
-  //   const targetId = targetHash ? targetHash.substring(1) : null;
-  //   console.log('targetHash found:'  + targetHash)
-  //   if (targetId) {
-  //     pollForElement(targetId); // Start polling for the target element
-  //   }
-  // };
-
-  // handleHashNavigation(); // Run on page load
-  // window.addEventListener('hashchange', handleHashNavigation); // Run on hash change
+	if (rememberedVariant) {
+    // Construct the selector using the rememberedVariant
+    const selector = `#variant-${rememberedVariant}.category__item`;
+    
+    // Scroll to the element with the constructed selector
+    const targetElement = document.querySelector(selector);
+		console.log(`[TARGET] ${targetElement}`);
+		
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'auto' }); // or smooth
+      console.log(`Scrolled to element with selector: ${selector}`);
+    } else {
+      console.log(`Element with selector ${selector} not found.`);
+    }
+  } else {
+    console.log('No rememberedVariant found in localStorage.');
+  }
 });
