ad139f58c21bad41cb9eb65e14aa50d24b1a07fb
Leland Reardon <leland6925@gmail.com>
2024-08-28 16:13:23 -0700

    recommended changes

--- assets/back-button-product-scroll.js
+++ assets/back-button-product-scroll.js
@@ -1,23 +1,14 @@
-// window.onload = () => {
-document.addEventListener('DOMContentLoaded', () => {
-  const rememberedVariant = localStorage.getItem('rememberedVariant');
-	console.log('rememberedVariant:', rememberedVariant);
-	if (rememberedVariant) {
-    // Construct the selector using the rememberedVariant
+document.addEventListener("DOMContentLoaded", () => {
+  const rememberedVariant = localStorage.getItem("rememberedVariant");
+  console.log("rememberedVariant:", rememberedVariant);
+  if (rememberedVariant) {
     const selector = `#variant-${rememberedVariant}.category__item`;
-    
-    // Scroll to the element with the constructed selector
+
     const targetElement = document.querySelector(selector);
-		console.log(`[TARGET] ${targetElement}`);
-		
+    console.log(`[TARGET] ${targetElement}`);
+
     if (targetElement) {
-      targetElement.scrollIntoView({ behavior: 'auto' }); // or smooth
-      console.log(`Scrolled to element with selector: ${selector}`);
-    } else {
-      console.log(`Element with selector ${selector} not found.`);
+      targetElement.scrollIntoView({ behavior: "auto" }); // or smooth
     }
-  } else {
-    console.log('No rememberedVariant found in localStorage.');
   }
 });
-// }
