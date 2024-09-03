a3fb59c2d2a8ab2a8a00555f6a5a1292dea9580a
Leland Reardon <leland6925@gmail.com>
2024-09-03 00:30:09 -0700

    .

--- assets/back-button-product-scroll.js
+++ assets/back-button-product-scroll.js
@@ -17,7 +17,7 @@ function runOnStart() {
 }
 
 if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', runOnStart);
+    document.addEventListener('load', runOnStart);
 } else {
     runOnStart();
 }
