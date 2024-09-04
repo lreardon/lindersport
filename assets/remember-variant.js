ec31a02c60574e4c596c6b38f7c9b178cd4f8e2f
Leland Reardon <leland6925@gmail.com>
2024-09-04 15:33:42 -0700

    fix: clear rememberedVariant after scrolling

--- assets/remember-variant.js
+++ assets/remember-variant.js
@@ -1,7 +1,3 @@
 function updateRememberedVariant(variantId) {
-	console.log(`[REMEMBER] ${variantId}`);
 	localStorage.setItem('rememberedVariant', variantId);
-
-	const rememberedVariantId = localStorage.getItem('rememberedVariant');
-	console.log(`[TRACKING] ${rememberedVariantId}`);
 }
